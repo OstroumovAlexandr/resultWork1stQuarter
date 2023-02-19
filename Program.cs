08a3ae210ad040f36d297f525369e3abc5b75cb2
Alexandr Ostroumov <m0nstrya9a@gmail.com>
2023-02-19 14:18:20 +0300

    Add file Program.cs

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿string[] ReadyArray = { "1234", "1567", "-2", "computer science", ";-)" };
+int size = GetCountArray(ReadyArray);
+string[] NewArray = CreateNewArray(ReadyArray, size);
+Console.WriteLine($"[{String.Join(", ", (ReadyArray))}]");
+Console.Write($"[{String.Join(", ", (NewArray))}]");
+
+//метод проверки массива на соответствие условиям
+//возвращаем счетик для нового массива
+int GetCountArray(string[] ReadyArray)
+{
+    int count = 0;
+    foreach (string el in ReadyArray)
+    {
+        if (el.Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+// //Метод создания массива по условиям задачи
+string[] CreateNewArray(string[] ReadyArray, int size)
+{
+    string[] NewArray = new string[size];
+    int count = 0;
+    for (int i = 0; i < ReadyArray.Length; i++)
+    {
+        if (ReadyArray[i].Length <= 3)
+        {
+            NewArray[count] = ReadyArray[i];
+            count++;
+        }
+    }
+    return NewArray;
+}
